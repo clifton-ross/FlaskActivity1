5afd53d9ac6d4dca98a2c34140c55682a5f0be9f
clifton-ross <117618976+clifton-ross@users.noreply.github.com>
2022-11-07 15:38:19 +1100

    Create BannerArea.py

---
+++ BannerArea.py
@@ -0,0 +1,8 @@
+##
+## This function returns a uniform heading for our little website.
+## The function returns a string value with embedded HTML and CSS markup
+## Page content should be concatenated with the output from the function within Flask
+##
+
+def BannerArea():
+    return '<h1 style="border: 3px solid powderblue; padding: 10px; color: blue;"> VCE Applied Computing!</h1>'
